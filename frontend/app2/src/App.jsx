af56a17e0f4e99f4c0fab2f37f8c75bb2a63d728
Ginanjar S.B <egin066@gmail.com>
2024-11-16 10:17:07 +0700

    init project

---
+++ frontend/app2/src/App.jsx
@@ -0,0 +1,23 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const navigateBack = () => window.location.replace("http://localhost:3000");
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>WELCOME TO APP2</h1>
+      <div className="card">
+        <button onClick={navigateBack}>Logout</button>
+      </div>
+      <p className="read-the-docs">@Ginsebu</p>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/gateway-app/src/App.jsx
@@ -0,0 +1,43 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const listApps = [
+    {
+      name: "app1",
+      url: "http://localhost:3001",
+    },
+    {
+      name: "app2",
+      url: "http://localhost:3002",
+    },
+  ];
+
+  const navigateTo = (app) => {
+    if (Object.keys(app).length > 0 && app.url)
+      window.location.replace(app.url);
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Gateway App</h1>
+      <div className="card">
+        {listApps.map((app, index) => (
+          <button className="btn-navigation" key={index} onClick={() => navigateTo(app)}>
+            {app.name.toUpperCase()}
+          </button>
+        ))}
+      </div>
+      <p className="read-the-docs">
+        @Ginsebu
+      </p>
+    </>
+  );
+}
+
+export default App;
